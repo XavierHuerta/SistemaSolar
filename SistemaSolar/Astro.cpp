e7ac21754a12d410d8aea77cd41d269ed050737b
Xavier Huerta <kirbyxavier2@outlook.com>
2023-09-05 08:37:39 -0600

    Intento de integrar Astro como clase abstracta

--- SistemaSolar/Astro.cpp
+++ SistemaSolar/Astro.cpp
@@ -1,1 +1,7 @@
 #include "Astro.h"
+
+Astro::Astro() {
+	pos = { 0,0,0 };
+	Circulo c(facEscala,pos);
+}
+
--- SistemaSolar/Astro.h
+++ SistemaSolar/Astro.h
@@ -1,5 +1,27 @@
 #pragma once
+
+#include <vector>
+
+#include "Circulo.h"
+
+using namespace std;
+
 class Astro
 {
+protected:
+	Circulo skin;
+	int radio;
+	vector <int> pos;
+	vector <float> color;
+
+	int facEscala;
+	float alfa;
+	vector <vector <float>>* matrizModel;//referencia a la matriz a
+
+	void draw();
+	void update();
+
+public:
+	Astro();
 };
 
